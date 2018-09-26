418939c735b9ac42d88fd76e1f5a91770ccb2476
Mack Goodstein <mack@eol-endeavour.eol.ucar.edu>
2018-09-26 16:49:52 -0600

    Updated script, it is now terrible but maybe works

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 # Setup shell
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 chsh -s /bin/zsh
 
-# Download dotfiles
-git clone https://github.com/mackmgg/mydotfiles.git ~/.dotfiles
-cp ~/.dotfiles/* ~/
+# Download dotfiles. This is a terrible kludge right now
+git clone https://github.com/mackmgg/mydotfiles.git "$HOME/.dotfiles"
+for rcfile in $(find $HOME/.dotfiles -name '.*' -mindepth 1 | cut -c $(echo "$HOME/.dotfiles/" | wc -c)-); do
+  echo $HOME/.dotfiles/$rcfile $HOME/${rcfile}
+done
 
 # Setup homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
